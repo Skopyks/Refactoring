96ce61cb01db627f45db02688e89fdd81a2d1bd8
Ipz234_khbp <ipz234_khbp@student.ztu.edu.ua>
2025-02-09 17:42:59 +0200

    Використання обчислень з Task 2

--- Task3.py
+++ Task3.py
@@ -1,9 +1,6 @@
+results = list(operations.values())
 
-results = [15, 5, 50, 2.0, 100000, 2, 0]
-
-count = len(results)
-
-print(f"Кількість елементів у списку: {count}")
+print(f"Кількість елементів у списку: {len(results)}")
 
 even_elements = [x for x in results if x % 2 == 0]
 print("Парні елементи списку:", even_elements)
