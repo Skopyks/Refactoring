8420bd21a869c5a8f1db9d576edc5a990bb81221
Ipz234_khbp <ipz234_khbp@student.ztu.edu.ua>
2025-02-09 17:34:39 +0200

    Логічніші назви змінних\

--- Task2.py
+++ Task2.py
@@ -1,13 +1,10 @@
-a = 10
-b = 5
+num1, num2 = 10, 5
 
-addition = a + b
-subtraction = a - b
-division = a / b
-power = a ** b
-integer_division = a // b
-remainder = a % b
-
-results = [addition, subtraction, division, power, integer_division, remainder]
-
-print(results)
+operations = {
+    "addition": num1 + num2,
+    "subtraction": num1 - num2,
+    "division": num1 / num2,
+    "power": num1 ** num2,
+    "integer_division": num1 // num2,
+    "remainder": num1 % num2
+}
